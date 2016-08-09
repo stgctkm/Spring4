51b95d5775461251724f70a2d2e3e5a53b4351c5
setoguchi takuma <setoguchi.takuma@gmail.com>
2016-08-09 12:12:21 +0900

    move setting files to resource directory

--- mybatis/src/main/java/sample/Main.java
+++ mybatis/src/main/java/sample/Main.java
@@ -1,29 +1,23 @@
 package sample;
 
-import java.util.List;
-
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionStatus;
-
-import sample.config.DataSourceConfig;
-import sample.config.MyBatisConfig;
 import sample.mybatis.business.domain.Pet;
 import sample.mybatis.business.service.PetDao;
+
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
     	//Springのコンテナを生成        
     	//JavaConfigでBean定義した場合
-        ApplicationContext ctx = new AnnotationConfigApplicationContext(
-                DataSourceConfig.class, MyBatisConfig.class);
+//        ApplicationContext ctx = new AnnotationConfigApplicationContext(
+//                DataSourceConfig.class, MyBatisConfig.class);
 
     	//Springのコンテナを生成        
     	//XMLでBean定義した場合
-//        ApplicationContext ctx = new ClassPathXmlApplicationContext("sample/config/spring-mybatis.xml");
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("sample/config/spring-mybatis.xml");
         
         
         PetDao dao = ctx.getBean(PetDao.class);
--- mybatis/src/main/java/sample/config/MyBatisConfigForMapper.java
+++ mybatis/src/main/java/sample/config/MyBatisConfigForMapper.java
@@ -2,18 +2,15 @@ package sample.config;
 
 import javax.sql.DataSource;
 
-import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
-import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
